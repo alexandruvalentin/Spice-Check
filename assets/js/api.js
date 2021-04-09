4b3afec46ac20cb3514b48284103fca12cdea421
alexandruvalentin <alexandru22valentin@gmail.com>
2021-04-09 21:10:40 +0000

    Update fetchAPI and generateHTML functions to display error messages if needed

--- assets/js/api.js
+++ assets/js/api.js
@@ -13,30 +13,25 @@ searchForm.addEventListener('submit', (e) => {
 
 async function fetchAPI (){
     const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${app_ID}&app_key=${app_KEY}&to=20`;
-    const response = await fetch(baseURL);
-    const data = await response.json();
+    await fetch(baseURL)
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
     generateHTML(data.hits);
-    console.log(data);
-}
-
-/* Console logging the constant response = await fetch(baseURL) was returning the following error: 
-"Access to fetch at 'https://api.edamam.com/search?q=chicken&app_id=c5791d75&app_key=c8d9601d5a776cfa5a818efd514a7cd4'
-from origin 'https://8000-blush-skunk-4es1078f.ws-eu03.gitpod.io' has been blocked by CORS policy:
-No 'Access-Control-Allow-Origin' header is present on the requested resource.
-If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled."
-    
-Fixed it (temporarely) by installing the chrome extension Moesif Origin & CORS Changer
-(https://chrome.google.com/webstore/detail/moesif-origin-cors-change/digfbfaphojjndkpccljibejjbppifbc/related)
-*/
-
-/* Function to loop through the array of generated results */
+    })
+    .catch(function(error) {
+    alert(error);
+    });
+    }
 
 function generateHTML(results) {
     recipeContainer.classList.remove('before');
-    let generatedHTML = '';
-    results.map(result => {
-        generatedHTML +=
-        `
+        if(results.length == 0) {
+            searchResult.innerHTML = "<h3>No results found! Try again...</h3>"
+        } else {
+    const generatedHTML = results.map(result => {
+    return `
         <div class="search__result__item">
         <img src="${result.recipe.image}" alt="Recipe Image" />
         <div class="result__item__details">
@@ -48,7 +43,8 @@ function generateHTML(results) {
         <p class="result__item__data">Calories: ${result.recipe.calories.toFixed(0)}</p>
         <p class="result__item__data">Diet Labels: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : 'No Data Found'}</p>
         </div>
-        `
-    })
+    `
+    }).join("")
     searchResult.innerHTML = generatedHTML;
-};
+    }
+    };
