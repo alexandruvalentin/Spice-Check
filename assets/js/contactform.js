e3bf1d1a0f4734c83db302faa17f6d59ec582a28
alexandruvalentin <alexandru22valentin@gmail.com>
2021-04-10 14:02:50 +0000

    Declare constants to grab elements and declare the form submit event listener

---
+++ assets/js/contactform.js
@@ -0,0 +1,13 @@
+// Credits for the isValidEmail function: https://gist.github.com/tmazur/3965625
+function isValidEmail(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+};
+
+const form = document.querySelector('form');
+const nameInput = document.querySelector('input[name="name"]');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("boo-ya");
+})
