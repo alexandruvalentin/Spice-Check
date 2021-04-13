5e225cc675024cfa9fdd20858212c225b68c9311
alexandruvalentin <alexandru22valentin@gmail.com>
2021-04-13 16:02:26 +0000

    Fix typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 /*  Function to turn the hamburger menu bars into a cross when actioned;
 credits: https://www.geeksforgeeks.org/how-to-convert-the-hamburger-icon-of-navigation-menu-to-x-on-click/ */
-const menu = document.querySelector('#mobile-menu')
+const menu = document.querySelector('#mobile__menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener('click', function () {
