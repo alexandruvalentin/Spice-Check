8c3e2c9f56a108e13f8b20f6204b2b99d0789398
alexandruvalentin <alexandru22valentin@gmail.com>
2021-04-15 08:28:32 +0000

    Add function to hide menu upon clicking menu item to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
-/*  Function to turn the hamburger menu bars into a cross when actioned;
-credits: https://www.geeksforgeeks.org/how-to-convert-the-hamburger-icon-of-navigation-menu-to-x-on-click/ */
 const menu = document.querySelector('#mobile__menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const navLogo = document.querySelector('#navbar__logo')
 
+//  Turn the hamburger menu bars to X when actioned; 
+//  credits: https://www.geeksforgeeks.org/how-to-convert-the-hamburger-icon-of-navigation-menu-to-x-on-click/
 menu.addEventListener('click', function () {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
 })
+
+// Hide the mobile menu upon menu item click;
+const hideMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+}
+
+menuLinks.addEventListener('click', hideMenu);
+navLogo.addEventListener('click', hideMenu);
